3f605834c4f9f72b563cb09a949115334cf56086
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2020-12-16 17:57:02 +0200

    first commit

---
+++ Sources/MyClass.swift
@@ -0,0 +1,12 @@
+//
+//  MyClass.swift
+//  MyDictionaryFramework
+//
+//  Created by Дмитрий Чумаков on 16.12.2020.
+//
+
+import Foundation
+
+public class MyClass {
+    
+}
---
+++ Sources/MyDictionaryFramework.h
@@ -0,0 +1,16 @@
+//
+//  MyDictionaryFramework.h
+//  MyDictionaryFramework
+//
+//  Created by Дмитрий Чумаков on 16.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDictionaryFramework.
+FOUNDATION_EXPORT double MyDictionaryFramework_VersionNumber;
+
+//! Project version string for MyDictionaryFramework.
+FOUNDATION_EXPORT const unsigned char MyDictionaryFramework_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDictionaryFramework_iOS/PublicHeader.h>
